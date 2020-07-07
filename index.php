92b6f6c4c1ec1f0a66e5f8719b777bd1aacdbb05
rahulwebmyne <67747183+rahulwebmyne@users.noreply.github.com>
2020-07-07 15:08:11 +0530

    Update index.php

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 PLugin Name: Github Custom Plugin Update
 Plugin URI: http://www.webmyne.com/
 Description: Github Custom Plugin Update
-Version: 5.0
+Version: 6.0
 Author: Webmyne
 Author URI: http://www.webmyne.com/
 */
